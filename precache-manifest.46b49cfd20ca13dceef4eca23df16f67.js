071244e7a263fa2b8319efc8e07b20ad7c235b6c
Takumi Adachi <takumiadachi@Takumis-MBP.fios-router.home>
2021-10-11 22:29:47 -0400

    Updates

---
+++ precache-manifest.46b49cfd20ca13dceef4eca23df16f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddad2c7557922c13a517",
+    "url": "/expense-tracker/static/css/main.87670c97.chunk.css"
+  },
+  {
+    "revision": "ddad2c7557922c13a517",
+    "url": "/expense-tracker/static/js/main.34d37cbc.chunk.js"
+  },
+  {
+    "revision": "5baca6083ea1223d23cf",
+    "url": "/expense-tracker/static/js/runtime~main.ae18d0c9.js"
+  },
+  {
+    "revision": "b29a51c75a76c1ee0283",
+    "url": "/expense-tracker/static/js/2.9099ca73.chunk.js"
+  },
+  {
+    "revision": "e616c1c34b43ce21257d8276520200d2",
+    "url": "/expense-tracker/static/media/test1.e616c1c3.jpg"
+  },
+  {
+    "revision": "4138e654e0d936874f958f1410c5dc8e",
+    "url": "/expense-tracker/index.html"
+  }
+];
